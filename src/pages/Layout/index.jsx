4812e43d9b179e1853d282522e497a4d6548ca11
MiKoChan <1607250879@qq.com>
2026-06-14 21:15:09 +0800

    新增layout、

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,28 +1,52 @@
-import { Button } from "antd-mobile";
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBillList } from "@/store/modules/billReducer";
+import { TabBar } from "antd-mobile";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+import "./index.scss";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
 
 export default function Layout() {
   const dispatch = useDispatch();
-  const { billList } = useSelector((state) => state.bill);
+  // const { billList } = useSelector((state) => state.bill);
   useEffect(() => {
     dispatch(fetchBillList());
   }, [dispatch]);
+
   return (
-    <div>
-      Layout
-      <Outlet />
-      <Button color="primary">qweqwre</Button>
-      <div className="purple">
-        <Button color="primary">asdqw</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
       </div>
-      <ul>
-        {billList.map((item) => {
-          <li key={item.id}>{item.useFor}</li>;
-        })}
-      </ul>
+      <div className="footer">
+        <TabBar>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   );
 }
